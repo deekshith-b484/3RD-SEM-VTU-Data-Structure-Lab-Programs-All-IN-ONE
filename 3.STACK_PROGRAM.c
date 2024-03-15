31aa1a5c50132526d29fb0e4af631201b82d05f9
NANDAN R <nandanr7211@gmail.com>
2024-03-15 18:18:13 +0530

    Update 3.STACK_PROGRAM.c

--- 3.STACK_PROGRAM.c
+++ 3.STACK_PROGRAM.c
@@ -1,48 +1,69 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define size 100
-int s[size];
-int top = -1;
-void push();
-void pop();
-void display();
-int main(){
+#include<string.h>
+#define MAX 100
+ int s[MAX];
+ int top=-1;
+
+ void push();
+ void pop();
+ void display();
+ void palindrome();
+
+ int main(){
     int ch;
     while(1){
-        printf("Enter the choice\n 1.push\n 2.pop\n 3.display\n 4.exit\n");
+        printf("\nENTER THE CHOICE\n 1.Push\n 2.pop\n 3.display \n 4.palindrome\n");
         scanf("%d",&ch);
 
         switch(ch){
-            case 1:push();
-                   break;
-            case 2:pop();
-                  break;
-            case 3:display();
-                  break;
-            case 4:exit(0);
-                  break;
+            case 1 : push();break;
+            case 2 : pop();break;
+            case 3 : display();break;
+            case 4 : palindrome();break;
         }
+    }
+    return 0;
+ }
 
-    }
-}
-void push(){
+ void push(){
     int x;
-    if(top==size-1){
-        printf("overflow");
+
+    if(top==MAX-1){
+        printf("Overflow");
     }else{
-        printf("enter the element:\n");
-        scanf("%d",&x);
-        top = top+1;
-        s[top]=x;
-    }
-}
-void pop(){
-    printf("poped element is \n%d",s[top]);
+    printf("Enter to push element:");
+    scanf("%d",&x);
+    top=top+1;
+    s[top]=x;
+ }
+ }
+  
+ void pop(){
+    if(top==-1){
+       printf("Underflow");
+    }else{
+    printf("The poped element is :%d",s[top]);
     top=top-1;
-}
-void display()
-{
-    for(int i=top;i>=0;--i){
+ }
+ }
+
+ void display(){
+    for(int i=top;i>=0;i--){
         printf("|%d|\n",s[i]);
-}
-}
+    }
+ }
+  void palindrome(){
+
+    char a[10],b[10];
+    printf("Enter the str\n");
+    fflush(stdin);
+    gets(a);
+    strcpy(b,a);
+    strrev(b);
+    if(strcmp(a,b)==0){
+    printf("this is palindrome");
+    }else{
+    printf("This is not palindrome");
+  }
+  }
